1cdcb44dcd650062be890628b7bad738e6524d7e
manoharmeena123 <manoharmeena245@Gmail.com>
2023-06-30 19:01:46 +0530

    Solidity full stack

---
+++ lottery-boilerplate/lottery/deploy.js
@@ -0,0 +1,80 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const { Web3 } = require('web3');
+const { abi, evm } = require('./compile');
+
+const provider = new HDWalletProvider(
+  "flash vote session fluid usage supply inject mule hair drift cinnamon camp",
+  // remember to change this to your own phrase!
+  "https://sepolia.infura.io/v3/455ac02833ac499280f1d41ef8a135dc"
+  // remember to change this to your own endpoint!
+);
+const web3 = new Web3(provider);
+ 
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+ 
+  console.log('Attempting to deploy from account', accounts[0]);
+ 
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
+    .send({ gas: '1000000', from: accounts[0] });
+ 
+  console.log(JSON.stringify(abi));
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
+
+abi[
+  ({
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor",
+  })
+];
---
+++ lottery-react-boilerplate/lottery-react/src/App.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from "react";
+import web3 from "./web3";
+import lottery from "./lottery";
+ 
+function App() {
+  const [manager, setManager] = useState("");
+  const [players, setPlayers] = useState([]);
+  const [balance, setBalance] = useState("");
+  const [value, setValue] = useState("");
+  const [message, setMessage] = useState("");
+ 
+  useEffect(() => {
+    async function fetchData() {
+      const manager = await lottery.methods.manager().call();
+      const players = await lottery.methods.getPlayers().call();
+      const balance = await web3.eth.getBalance(lottery.options.address);
+ 
+      setManager(manager);
+      setPlayers(players);
+      setBalance(balance);
+    }
+ 
+    fetchData();
+  }, []);
+ 
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+    setMessage("Waiting on transaction success...");
+ 
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(value, "ether"),
+    });
+ 
+    setMessage("You have been entered!");
+  }
+ 
+
+  async function handleClick() {
+    const accounts = await web3.eth.getAccounts();
+ 
+    setMessage("Waiting on transaction success...");
+ 
+    await lottery.methods.pickWinner().send({
+      from: accounts[0],
+    });
+ 
+    setMessage("A winner has been picked!");
+  }
+ 
+  return (
+    <div>
+      <h2>Lottery Contract</h2>
+      <p>
+        This contract is managed by {manager}. There are currently{" "}
+        {players.length} people entered, competing to win{" "}
+        {web3.utils.fromWei(balance, "ether")} ether!
+      </p>
+ 
+      <hr />
+      <form onSubmit={handleSubmit}>
+        <h4>Want to try your luck?</h4>
+        <div>
+          <label>Amount of ether to enter</label>
+          <input
+            value={value}
+            onChange={(event) => setValue(event.target.value)}
+          />
+        </div>
+        <button>Enter</button>
+      </form>
+ 
+      <hr />
+ 
+      <h4>Ready to pick a winner?</h4>
+      <button onClick={handleClick}>Pick a winner!</button>
+ 
+      <hr />
+ 
+      <h1>{message}</h1>
+    </div>
+  );
+}
+ 
+export default App;
